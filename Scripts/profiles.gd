916e18a3f8db29d8a98a906249e2c7374642b475
GenesisJayus <paayassondel@gmail.com>
2026-09-19 01:12:27 +0800

    Savefile Screen Change
    Added a box to type name
    started connecting the inputs to save files

--- Scripts/profiles.gd
+++ Scripts/profiles.gd
@@ -31,13 +31,30 @@ func label_content() -> void:
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("uid://ckxcjkvo71ibl")
 
-
+var which_save = 0
 func _on_s1b_pressed() -> void:
 	name_input.visible = true
+	which_save = 1
 func _on_s2b_pressed() -> void:
 	name_input.visible = true
+	which_save = 2
 func _on_s3b_pressed() -> void:
 	name_input.visible = true
+	which_save = 3
 
 func _on_nameback_pressed() -> void:
+	var a = $Panel/LineEdit
 	name_input.visible = false
+	a.clear()
+func _on_confirm_pressed() -> void:
+	var named = ($Panel/LineEdit).text.capitalize()
+	var welcome: Label = $Panel/Label
+	if which_save == 1:
+		Save1.stats["name"] = named
+		welcome.text = "Welcome " + str(Save1.stats["name"])
+	elif which_save == 2:
+		Save2.stats["name"] = named
+		welcome.text = "Welcome " + str(Save2.stats["name"])
+	else:
+		Save3.stats["name"] = named
+		welcome.text = "Welcome " + str(Save3.stats["name"])
