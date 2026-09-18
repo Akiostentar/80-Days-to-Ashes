f024ba291487546760b17dba4847f9be1eda7240
GenesisJayus <paayassondel@gmail.com>
2026-09-18 23:03:14 +0800

    Backend Set-up
    Setting up the base for the save features in the game

---
+++ Scripts/Save1.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var player_name: String = "\n No Save Data"
+var player_health: int = 100
+var player_strength: int = 5
+var player_defense: int = 5
+var player_agility: int = 10
+var player_intelligence: int = 50
+var player_dexterity: int = 10
+
+func data() -> Dictionary:
+	return {
+		"name": player_name,
+		"hp": player_health,
+		"str": player_strength,
+		"def": player_defense,
+		"agi": player_agility,
+		"int": player_intelligence,
+		"dex": player_dexterity
+	}
---
+++ Scripts/profiles.gd
@@ -0,0 +1,27 @@
+extends Control
+
+const SV1 = preload("uid://dpikte7ybok0u")
+const SV2 = preload("uid://cy7t10cp5trqt")
+const SV3 = preload("uid://ba8qubhgxsrt6")
+var Save1 = SV1.new()
+var Save2 = SV2.new()
+var Save3 = SV3.new()
+
+@onready var S2: Label = $HBoxContainer/VBoxContainer2/Label2
+@onready var S3: Label = $HBoxContainer/VBoxContainer3/Label2
+@onready var S1: Label = $HBoxContainer/VBoxContainer/Label2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	label_content()
+
+func label_content():
+	var data1 = Save1.data()
+	var data2 = Save2.data()
+	var data3 = Save3.data()
+	S1.text = data1["name"]
+	S2.text = data2["name"]
+	S3.text = data3["name"]
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("uid://ckxcjkvo71ibl") # Replace with function body.
