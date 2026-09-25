46cb5808549cfd97fdcb455213d3262ae0a06648
NoobCatXD <thomaserondampac@gmail.com>
2026-09-25 13:02:15 +0800

    Added Day 2 Dialogues for Companion
    Added Day 2 dialogues for our Companion as well as Dungeon Dialogues

--- Scripts/button_9.gd
+++ Scripts/button_9.gd
@@ -7,9 +7,8 @@ func _ready() -> void:
 	add_to_group("dialogue_buttons")
 	pressed.connect(_on_button_pressed)
 
-func _on_button_pressed() -> void:
-	get_tree().call_group("dialogue_buttons", "hide")
-	%FlashbackLabel.text = "Before entering the dungeon"
+func play_fade_overlay(title_text: String) -> void:
+	%FlashbackLabel.text = title_text
 	%FadeOverlay.modulate.a = 0.0
 	%FadeOverlay.show()
 	
@@ -23,8 +22,13 @@ func _on_button_pressed() -> void:
 	fade_in.tween_property(%FadeOverlay, "modulate:a", 0.0, 1.0)
 	await fade_in.finished
 	%FadeOverlay.hide()
+
+func _on_button_pressed() -> void:
+	get_tree().call_group("dialogue_buttons", "hide")
 	
-	var flashback_sequence: Array[Dictionary] = [
+	await play_fade_overlay("Before entering the dungeon")
+	
+	var sequence_part_1: Array[Dictionary] = [
 		{
 			"name": "Main Character",
 			"text": "Hmmm, It seems people here are forming teams with others",
@@ -49,9 +53,23 @@ func _on_button_pressed() -> void:
 			"portrait": character_texture,
 			"monologue": false
 		},
+		
+		#Lines for dungeon: (start off with you then companion then switch)
+		#so whats your name?
+		#oh im [insert main character name]
+		#Hello [ur name], im [insert companion name]
+		#Well hello, so uhm.. can you help me understand the dungeon?
+		#The dungeon is labyrinth full of monsterbut also hide all kinds of valuables, thats about it really
+		#Wait huh why are monsters still appearing here?
+		#Well we think that at the lowest floor holds a magic core that creates monsters and items each day.
+		#Okay has anybody tried seeing what happens to dungeon when the time reaches midnight?
+		#People have tried and are never to be seen again
+		#Hmmm...
+		#Well just focus on getting as much stuff here before they close the dungeon
+		
 		{
 			
-			"text": "(after the dungeon)",
+			"text": "After the dungeon",
 			"monologue": true
 			
 		},
@@ -146,7 +164,79 @@ func _on_button_pressed() -> void:
 		{
 			"text": "Can I really save my family.",
 			"monologue": true
+		},
+]
+
+	%DialoguePanel.start_sequence(sequence_part_1, false)
+	await %DialoguePanel.dialogue_ended
+
+	await play_fade_overlay("Day 2")
+
+	var sequence_part_2: Array[Dictionary] = [
+		{
+			"name": "Main Character",
+			"text": "Nngh... Ugh",
+			"portrait": null,
+			"monologue": false
+		},
+		{
+			"name": "Main Character",
+			"text": "Wh-What time is it?",
+			"portrait": character_texture, 
+			"monologue": false
+		},
+		{
+			"name": "Main Character",
+			"text": "Dammit I woke up late, I need to hurry and meet [insert companion name]",
+			"portrait": character_texture,
+			"monlogue": false
+		},
+		{
+			"name": "Main Character",
+			"text": "I hope he's still there waiting for me",
+			"portrait": character_texture,
+			"monologue": false
+		},
+		{
+			"text": "15 minutes later...",
+			"monologue": true
+		},
+		{
+			"name": "Companion",
+			"text": "*Sign*, You're late",
+			"portrait": character_texture,  
+			"monologue": false  
+		},
+		{
+			"name": "Main Character",
+			"text": "Haha... Sorry still getting used to my bed",
+			"portrait": character_texture,
+			"monologue": false
+		},
+		{
+			"name": "Companion",
+			"text": "Well atleast you did'nt make wait that long, and look the dungeon is about to open, lets go",
+			"portrait": character_texture,
+			"monologue": false
+		},
+		{
+			"name": "Main Character",
+			"text": "Right behing you",
+			"portrait": character_texture,
+			"monologue": false
 		}
+		
+		#Lines for dungeon:
+		#Ever heard of [insert evil guild name]
+		#Oh them? they have destroyed countless villages and enslaved so many people
+		#If everyone knows what they did, why can't the higher ups to something?
+		#It's due to their connections with other cities and powerful people, one wrong move and you could start a war 
+		#(Can I really beat them? If they have all this power, what can I do with this frail body of mine)
+		#Their boss is also very strong, when he came here to try out our dungeon, he went downn 128 floors in one day 
+		#And who is he?
+		#His name is [Insert Evil Guild Boss Name]
+		#[Insert Evil Guild Boss name], huh (I'll make sure to remember that name)
+		
 	]
 	
-	%DialoguePanel.start_sequence(flashback_sequence, true)
+	%DialoguePanel.start_sequence(sequence_part_2, true)
